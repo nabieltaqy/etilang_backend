a8d5eda2e66d3288b3a7175eb2e98be3557f2f33
nabieltaqy <nabieltaqy@gmail.com>
2025-05-17 15:54:02 +0700

    update env example untuk midtrans lupa kebawa

--- app/Http/Controllers/Api/MidtransController.php
+++ app/Http/Controllers/Api/MidtransController.php
@@ -26,11 +26,14 @@ class MidtransController extends Controller
         // Validasi input
         $request->validate([
             'ticket_id' => 'required|exists:tickets,id',
-            'amount' => 'required|numeric',
+            // 'amount' => 'required|numeric',
         ]);
 
+        $ticket = Ticket::with(['violation.violationType'])->find($request->ticket_id);
+        $max_fine = $ticket->violation->violationType->max_fine;
+
         // Cek apakah tiket sudah memiliki transaksi sebelumnya
-        $existingTransaction = Ticket::find($request->ticket_id)->transactions()->first();
+        $existingTransaction = Ticket::find($request->ticket_id)->transaction();
 
         // Jika ada transaksi yang sudah ada, hapus transaksi lama
         if ($existingTransaction) {
@@ -43,13 +46,12 @@ class MidtransController extends Controller
         $transactionDetails = [
             'transaction_details' => [
                 'order_id' => $orderId,
-                'gross_amount' => $request->amount,
+                'gross_amount' => $max_fine, // Jumlah total yang harus dibayar
             ],
             'ticket_id' => $request->ticket_id,
             'credit_card' => [
                 'secure' => true, // Menggunakan 3D Secure
             ],
-            // 'notification_url' => 'https://b991-103-233-100-204.ngrok-free.app/api/midtrans/callback',
         ];
 
         // Kirim request untuk transaksi ke Midtrans
@@ -69,75 +71,4 @@ class MidtransController extends Controller
         // Mengembalikan snap_token atau informasi transaksi lainnya
         return response()->json(['snap_token' => $response]);
     }
-
-
-    // mengambil status transaksi dari Midtrans
-
-    //     public function getTransactionStatus($orderId)
-    // {
-    //     try {
-    //         // Mendapatkan status transaksi berdasarkan order_id
-    //         $status = Transaction::status($orderId);
-
-    //         // Mengembalikan status transaksi dalam bentuk array
-    //         return response()->json(['status' => $status ], 200);
-
-    //     } catch (\Exception $e) {
-    //         return response()->json(['error' => $e->getMessage()], 500);
-    //     }
-    // }
-
-    // Untuk menerima dan menangani callback dari Midtrans
-    // public function handleCallback(Request $request)
-    // {
-    //     // Inisialisasi notifikasi dari Midtrans
-    //     $notification = new Notification();
-
-    //     // Ambil data dari notifikasi
-    //     $status = $notification->transaction_status; // Status transaksi dari Midtrans
-    //     $orderId = $notification->order_id;           // ID order dari transaksi
-    //     $paymentType = $notification->payment_type;   // Jenis pembayaran (e.g., credit_card, bank_transfer, etc.)
-
-    //     // Mencari transaksi berdasarkan order_id
-    //     $transaction = Transaction::where('order_id', $orderId)->first();
-
-    //     // Jika transaksi tidak ditemukan
-    //     if (!$transaction) {
-    //         return response()->json(['message' => 'Transaction not found'], 404);
-    //     }
-
-    //     // Update status transaksi berdasarkan status dari Midtrans
-    //     switch ($status) {
-    //         case 'capture':
-    //         case 'settlement':
-    //             // Pembayaran berhasil, update status menjadi 'success'
-    //             $transaction->status = 'success';
-    //             break;
-
-    //         case 'pending':
-    //             // Pembayaran sedang diproses, tetap dalam status 'pending'
-    //             $transaction->status = 'pending';
-    //             break;
-
-    //         case 'cancel':
-    //         case 'deny':
-    //         case 'expire':
-    //             // Pembayaran gagal, update status menjadi 'failed'
-    //             $transaction->status = 'failed';
-    //             break;
-
-    //         default:
-    //             // Status lainnya bisa ditangani sesuai kebutuhan
-    //             break;
-    //     }
-
-    //     // Menyimpan informasi metode pembayaran
-    //     $transaction->payment_method = $paymentType;
-
-    //     // Simpan perubahan status transaksi
-    //     $transaction->save();
-
-    //     // Kembalikan respons sukses
-    //     return response()->json(['message' => 'Transaction status updated']);
-    // }
 }
