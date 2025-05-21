f55a7d1dddca7a8950c51ab04321b2cba1380199
nabieltaqy <nabieltaqy@gmail.com>
2025-05-21 11:54:26 +0700

    menyesuaikan config mail

--- config/mail.php
+++ config/mail.php
@@ -43,7 +43,7 @@ return [
             // 'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 465),
-            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
