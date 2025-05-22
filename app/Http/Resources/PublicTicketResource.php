5b2e67587719546b8b7ca362c5a65f8866154522
nabieltaqy <nabieltaqy@gmail.com>
2025-05-22 14:39:57 +0700

    Menambahkan PublicTicketResource untuk mengubah format data tiket yang ditampilkan.

---
+++ app/Http/Resources/PublicTicketResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PublicTicketResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+return [
+            'id' => $this->id,
+            'violation' => new ViolationResource($this->whenLoaded('violation')),
+            'investigator' => new UserResource($this->whenLoaded('investigator')),
+            'color' => $this->color,
+            'status' => $this->status,
+            'deadline_confirmation' => $this->deadline_confirmation,
+            'hearing_schedule' => new HearingScheduleResource($this->whenLoaded('hearingSchedule')),
+            'vehicle' => new VehicleResource(optional($this->violation->vehicle)),
+            'payment' => new TransactionResource($this->whenLoaded('transaction')),
+            'activities' => ActivityResource::collection($this->whenLoaded('activities')),
+            'camera' => new CameraResource($this->whenLoaded('violation.camera')),
+            'appeal' => new AppealResource($this->whenLoaded('appeal')),
+            'violation_type' => new ViolationTypeResource($this->whenLoaded('violation.violationType')),
+            'transaction' => new TransactionResource($this->whenLoaded('transaction')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
